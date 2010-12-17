63366530001f8f1463816e4195b59edd2de3a7cc
dan2097 <dan2097@localhost>
2010-12-17 18:37:54 +0000

    Updated dependency on restlet to 2.0.3 Removed superfluous dependencies. The one call to sea36.util is replaced with code which I believe has the same effect Removed the ability to run the server without a servlet container. This didn't work very well and seems to be completely broken now and I don't think its worth maintaining

--- src/test/java/uk/ac/cam/ch/opsin/ws/APITest.java
+++ src/test/java/uk/ac/cam/ch/opsin/ws/APITest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package uk.ac.ch.opsin.ws;
+package uk.ac.cam.ch.opsin.ws;
 
 import java.io.IOException;
 import org.junit.Assert;
@@ -12,8 +12,8 @@ import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Preference;
 import org.restlet.data.Protocol;
-import org.restlet.data.Request;
-import org.restlet.data.Response;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.data.Status;
 
 /**
@@ -22,30 +22,30 @@ import org.restlet.data.Status;
  */
 public class APITest {
 
-   private static MediaType TYPE_SMILES = new MediaType(
-           "chemical/x-daylight-smiles");
+   private static MediaType TYPE_INCHI = new MediaType(
+           "chemical/x-inchi");
 
    @Test
    public void apiTest() throws IOException {
       Client client = new Client(Protocol.HTTP);
       Request req = new Request(Method.GET,
-                                "http://localhost:8989/opsin-ws/methane");
+                                "http://localhost:8989/opsin/methane");
       req.getClientInfo().getAcceptedMediaTypes().clear();
       req.getClientInfo().getAcceptedMediaTypes().add(new Preference<MediaType>(
-              TYPE_SMILES));
+    		  TYPE_INCHI));
       Response res = client.handle(req);
-      String smiles = res.getEntity().getText();
-      Assert.assertEquals("C(H)(H)(H)H", smiles);
+      String inchi = res.getEntity().getText();
+      Assert.assertEquals("InChI=1/CH4/h1H4", inchi);
    }
 
    @Test
    public void apiTestBadName() throws IOException {
       Client client = new Client(Protocol.HTTP);
       Request req = new Request(Method.GET,
-                                "http://localhost:8989/opsin-ws/BilboBaggins");
+                                "http://localhost:8989/opsin/BilboBaggins");
       req.getClientInfo().getAcceptedMediaTypes().clear();
       req.getClientInfo().getAcceptedMediaTypes().add(new Preference<MediaType>(
-              TYPE_SMILES));
+    		  TYPE_INCHI));
       Response res = client.handle(req);
       Assert.assertEquals(Status.CLIENT_ERROR_NOT_FOUND, res.getStatus());
    }
